e7c295f7560930a8d1f6ae374c6324bd28b6d662
Renan Oliveira <renanoliveira2199@gmail.com>
2018-08-16 21:34:54 -0300

    c

--- src/desafio.rb
+++ src/desafio.rb
@@ -25,3 +25,32 @@ end
 a = Desafio.new
 
 a.caixaEletronico
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    double valor = 20;
+    double latinha = 4.78;
+    double troco = valor-latinha;
+    int cedula = 0;
+
+    double a = 100;
+    int i = 0;
+    for(i=0;i<12;i++){
+        cedula = troco/a;
+        (int)troco = (int)troco%(int)a;
+        if(a>0){
+            printf("%f",cedula);
+            printf(" cedulas de R$%f",a,",00");
+            printf("\n");
+            if(a==50 || a == 5 || a == 0.25){
+                a/=2.5;
+                continue;
+            }else if(a==0.05){
+                a/=5;
+                continue;
+            }
+        }
+        a/=2;
+    }
+}
